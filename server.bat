10f48d77e27efd8fcf59c3688c64044dfe834ca8
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2011-07-03 15:24:43 -0400

    Windows requires named jars in classpath, not '*'

--- server.bat
+++ server.bat
@@ -1,1 +1,1 @@
-java -Djava.library.path=. -classpath * org.mozilla.javascript.tools.shell.Main bootstrap.js
+java -Djava.library.path=. -classpath finch.jar;js.jar;servlet-api-2.5.jar;jetty-all-7.2.2.v20101205.jar;json_simple-1.1.jar org.mozilla.javascript.tools.shell.Main bootstrap.js
